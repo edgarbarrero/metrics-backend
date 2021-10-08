a74f7d3420006e1f5a292037a27915e118d3fffd
Edgar Barrero <edgar.barrero.mate@gmail.com>
2021-10-07 19:22:15 -0500

    metric creation endpoint added

--- app/controllers/api/v1/metrics_controller.rb
+++ app/controllers/api/v1/metrics_controller.rb
@@ -7,4 +7,19 @@ class Api::V1::MetricsController < ApplicationController
       render json: @metrics
     end
   end
+
+  def create
+    metric = Metric.new(metric_params)
+    if metric.save
+      render json: { success: 200 }, status: 200
+    else
+      render json: { error: 500 }, status: 500
+    end
+  end
+
+  private
+
+  def metric_params
+    params.permit(:name, :value, :timestamp)
+  end
 end
