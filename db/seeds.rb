82d9b6e76d8a823c00970349c843c5fb0fb06f1c
Edgar Barrero <edgar.barrero.mate@gmail.com>
2021-10-08 20:18:32 -0500

    seeds for metrics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Metric.create(name: 'user_login', value: 10, timestamp: 2.hour.ago)
+Metric.create(name: 'user_login', value: 10, timestamp: 1.minute.ago)
+Metric.create(name: 'user_login', value: 10, timestamp: 2.minutes.ago)
